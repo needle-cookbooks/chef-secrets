0830b42c66ac5c775ca9f25acb1cccae4a4e0ac2
Cameron Johnston <cameron@needle.com>
2012-08-07 06:40:27 -0600

    Initial commit

---
+++ libraries/secrets.rb
@@ -0,0 +1,15 @@
+module Needle
+  module Secrets
+    class << self
+      def load(key,env)
+        begin
+          data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
+          Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
+        rescue => e
+          Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}")
+          raise
+        end
+      end
+    end
+  end
+end
---
+++ metadata.rb
@@ -0,0 +1,5 @@
+maintainer       "Needle Operations"
+maintainer_email "cookbooks@needle.com"
+license          "Apache 2.0"
+description      "Setec Astronomy"
+version          "0.0.1"
