57d3103821b4195e026c5a371b88bc07f19aaa48
Cameron Johnston <cameron@needle.com>
2012-08-07 07:04:05 -0600

    Needle::Secrets -> Secrets

--- libraries/secrets.rb
+++ libraries/secrets.rb
@@ -1,14 +1,12 @@
-module Needle
-  module Secrets
-    class << self
-      def load(key,env)
-        begin
-          data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
-          Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
-        rescue => e
-          Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}")
-          raise
-        end
+module Secrets
+  class << self
+    def load(key,env)
+      begin
+        data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
+        Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
+      rescue => e
+        Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}")
+        raise
       end
     end
   end
