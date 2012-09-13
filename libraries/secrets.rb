5ce089eb5c73ca61a30d0b3f0a2b09817f979246
Ben Belchak <ben@needle.com>
2012-09-13 13:49:24 -0600

    add retrying of decrypting secrets up to 5 times in the event of an error

--- libraries/secrets.rb
+++ libraries/secrets.rb
@@ -1,12 +1,16 @@
 module Secrets
   class << self
     def load(key,env)
-      begin
-        data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
-        Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
-      rescue => e
-        Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}")
-        raise
+      retries = 0
+      while retries < 5
+        begin
+          data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
+          Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
+          break
+        rescue => e
+          retries += 1
+          Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}\nretrying (1/5)")
+        end
       end
     end
   end
