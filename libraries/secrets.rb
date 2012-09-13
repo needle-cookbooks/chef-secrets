0547fd83689eedbc0fed16d6a564b72d54146ef8
Ben Belchak <ben@needle.com>
2012-09-13 14:11:27 -0600

    just a couple of logic fixes

--- libraries/secrets.rb
+++ libraries/secrets.rb
@@ -2,7 +2,7 @@ module Secrets
   class << self
     def load(key,env)
       retries = 0
-      while retries < 5
+      while retries <= 5
         begin
           data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
           Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
@@ -10,9 +10,10 @@ module Secrets
         rescue => e
           retries += 1
           Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}\nretrying (1/5)")
-          if retries >= 5
+          if retries == 5
             raise
           end
+          sleep 5
         end
       end
     end
