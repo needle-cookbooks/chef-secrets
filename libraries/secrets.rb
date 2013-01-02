7ab82bc31797cad62b65129213ed06f32700191a
Cameron Johnston <cameron@needle.com>
2013-01-02 11:24:40 -0700

    making Secrets.load use secrets::solo data bag item when running under chef-solo

--- libraries/secrets.rb
+++ libraries/secrets.rb
@@ -1,19 +1,23 @@
 module Secrets
   class << self
     def load(key,env)
-      retries = 0
-      while retries <= 5
-        begin
-          data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
-          return Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
-          break
-        rescue => e
-          retries += 1
-          Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}\nretrying (1/5)")
-          if retries == 5
-            raise
+      if Chef::Config.solo
+        return data_bag_item('secrets','solo')
+      else
+        retries = 0
+        while retries <= 5
+          begin
+            data_bag_key = Chef::EncryptedDataBagItem.load_secret(key)
+            return Chef::EncryptedDataBagItem.load('secrets', env, data_bag_key)
+            break
+          rescue => e
+            retries += 1
+            Chef::Log.fatal("Failed to load encrypted secrets:\n#{e.inspect}\nretrying (#{retries}/5)")
+            if retries == 5
+              raise
+            end
+            sleep 5
           end
-          sleep 5
         end
       end
     end
